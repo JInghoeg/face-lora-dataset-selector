a7f8df1141b4f8c969f7816d285760680843bfc9
K1nian <113687976+JInghoeg@users.noreply.github.com>
2026-09-21 21:17:56 +0800

    feat: add features/source_organizer/__init__.py

---
+++ features/source_organizer/__init__.py
@@ -0,0 +1,19 @@
+from .service import (
+    FEATURE_KEY,
+    OrganizerMove,
+    OrganizerPlan,
+    OrganizerResult,
+    build_plan,
+    execute_plan,
+    self_test,
+)
+
+__all__ = [
+    "FEATURE_KEY",
+    "OrganizerMove",
+    "OrganizerPlan",
+    "OrganizerResult",
+    "build_plan",
+    "execute_plan",
+    "self_test",
+]
