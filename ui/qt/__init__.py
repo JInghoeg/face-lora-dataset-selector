8272ea2414a4ba948ca414ae7e46f449e996ad05
K1nian <113687976+JInghoeg@users.noreply.github.com>
2026-09-22 21:52:55 +0800

    refactor: export reusable Qt presentation modules

--- ui/qt/__init__.py
+++ ui/qt/__init__.py
@@ -1,3 +1,11 @@
 from .duplicate_review import DuplicateReviewDialog
+from .image_preview import ImagePreview
+from .text_cleanup import SubtitleTab
+from .thumbnail import ThumbnailWorker
 
-__all__ = ["DuplicateReviewDialog"]
+__all__ = [
+    "DuplicateReviewDialog",
+    "ImagePreview",
+    "SubtitleTab",
+    "ThumbnailWorker",
+]
