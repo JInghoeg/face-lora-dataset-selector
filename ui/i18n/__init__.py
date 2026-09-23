56798f4ed1ccafe779c02b7abb30e629cb0fa278
K1nian <113687976+JInghoeg@users.noreply.github.com>
2026-09-23 20:40:55 +0800

    feat: add permanent Qt i18n foundation

---
+++ ui/i18n/__init__.py
@@ -0,0 +1,13 @@
+from .manager import (
+    SUPPORTED_LANGUAGES,
+    LanguageManager,
+    get_language_manager,
+    initialize_i18n,
+)
+
+__all__ = [
+    "SUPPORTED_LANGUAGES",
+    "LanguageManager",
+    "get_language_manager",
+    "initialize_i18n",
+]
